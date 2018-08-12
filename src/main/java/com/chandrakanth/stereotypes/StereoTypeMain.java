9affb46265002e9cc6efcb42d95dbefad7ce02cf
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-08-12 17:08:08 +0530

    M

--- src/main/java/com/chandrakanth/stereotypes/StereoTypeMain.java
+++ src/main/java/com/chandrakanth/stereotypes/StereoTypeMain.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class StereoTypeMain {
 
 	public static void main(String[] argz) {
-		AnnotationConfigApplicationContext annConfig = 
-				new AnnotationConfigApplicationContext(ConfigClass.class);
-		
-		ServiceClass sClass = (ServiceClass)annConfig.getBean(ServiceClass.class);
-		System.out.println(sClass);
-	}	
+		AnnotationConfigApplicationContext annConfig = new AnnotationConfigApplicationContext(ConfigClass.class);
 
+		ServiceClass sClass = (ServiceClass) annConfig.getBean(ServiceClass.class);
+		System.out.println(sClass);
+	}
 }
